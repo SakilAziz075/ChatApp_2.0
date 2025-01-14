3eae78022909693dbe35bb61d49096c007a501df
SakilAziz075 <sakilaziz12@gmail.com>
2025-01-14 13:08:49 +0530

    before reseting

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom/client';
 import AppRouter from './router';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<AppRouter />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+<AppRouter />);
